b75bdc2d2370cb6455ad910476149d5aaf6041b2
AdrianPrograma2 <apadgue2912@g.educaand.es>
2025-06-06 12:01:36 +0200

    Create MyJS.js

---
+++ Tema 3/Ejercicio3/MyJS.js
@@ -0,0 +1,11 @@
+function cambiaCoche(nombre) {
+    document.getElementById('miImagenCoche').src = nombre;
+}
+
+function cambiaSpiderman(nombre) {
+    document.getElementById('miImagenSpiderman').src = nombre;
+}
+
+function cambiarEstilo(nombreCSS) {
+    document.getElementById('hojaEstilo').href = nombreCSS;
+}
